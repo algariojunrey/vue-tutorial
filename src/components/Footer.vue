17958596994df6ec0502e678aaa06fd9a15ea8df
unknown <junrey.algario@gmail.com>
2022-12-09 17:25:13 +0800

    setup scss

---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <header>
+    <div class="footer">
+      <h3>Copyright 2022</h3>
+    </div>
+  </header>
+</template>
---
+++ src/components/Header.vue
@@ -0,0 +1,7 @@
+<template>
+  <header>
+    <div class="header">
+      <h3>Learning Vue Js</h3>
+    </div>
+  </header>
+</template>
