704ae8fe9ccbe04c79c65494b9e5e97605beefdb
unknown <junrey.algario@gmail.com>
2022-12-09 17:56:50 +0800

    add vue router

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,6 +2,11 @@
   <header>
     <div class="header">
       <h3>Learning Vue Js</h3>
+      <nav>
+        <a href="#">HOME</a>
+        <a href="#">ABOUT</a>
+        <a href="#">CONTACT US</a>
+      </nav>
     </div>
   </header>
 </template>
